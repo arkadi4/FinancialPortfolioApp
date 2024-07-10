d9bfcaaf838b9e735fb1eddfdc46e32c9239cb05
arkadi4 <arkadi40102@gmail.com>
2024-07-10 18:16:08 +0300

    fixes after review #1

--- app/src/main/java/com/example/financialportfolioapp/data/DataSample.kt
+++ app/src/main/java/com/example/financialportfolioapp/data/DataSample.kt
@@ -2,32 +2,33 @@ package com.example.financialportfolioapp.data
 
 import com.example.financialportfolioapp.domain.entities.Asset
 import com.example.financialportfolioapp.domain.entities.AssetInterface
-import com.example.financialportfolioapp.domain.entities.Currency
-import com.example.financialportfolioapp.domain.entities.InterestBearingBond
-import com.example.financialportfolioapp.domain.entities.Share
-import java.util.Date
+import com.example.financialportfolioapp.domain.entities.Bond
+import com.example.financialportfolioapp.domain.entities.Cash
+import com.example.financialportfolioapp.domain.entities.PortfolioItemInterface
+import com.example.financialportfolioapp.domain.entities.Stock
+import java.util.Calendar
 
 object DataSample {
     var assetList: MutableList<AssetInterface> = mutableListOf(
-        Asset(1, "USD", Date(), 30.0,),
-        Asset(2, "BYN", Date(), 1.0,),
-        Asset(3, "Minsk Tractors", Date(), 5_000.0,),
-        Asset(4, "Lufthansa", Date(), 10_000.0,),
-        Asset(5, "GP Morgan", Date(), 50_000.0,),
-        Asset(6, "Columbia Pictures", Date(), 25_000.0,),
+        Asset(1, "USD"),
+        Asset(2, "BYN"),
+        Asset(3, "Minsk Tractors"),
+        Asset(4, "Lufthansa"),
+        Asset(5, "GP Morgan"),
+        Asset(6, "Columbia Pictures"),
     )
 
-    var portfolioItemsList: MutableList<AssetInterface> = mutableListOf(
-        Currency(
-            1, "USD", Date(), 30.0, 10.5
+    var portfolioItemsList: MutableList<PortfolioItemInterface> = mutableListOf(
+        Cash(
+            1, "USD", 15.0, 3.20, "BYN", Calendar.getInstance()
         ),
-        Share(
-            2, "Minsk Tractors", Date(), 5_000.0, 15,
-            "Belarus", "industry"
+        Stock(
+            2, "Minsk Tractors", 20.0, 5_000.0, "BYN",
+            Calendar.getInstance(),
         ),
-        InterestBearingBond(
-            3, "GP Morgan", Date(), 50_000.0, 20,
-            "USA", "Different", 21_000.0, 1_000.0,
+        Bond(
+            3, "GP Morgan", 30.0, 50_000.0, "BYN",
+            Calendar.getInstance(), 51_000.0, 1_000.0,
         ),
     )
 }
--- app/src/main/java/com/example/financialportfolioapp/domain/entities/Asset.kt
+++ app/src/main/java/com/example/financialportfolioapp/domain/entities/Asset.kt
@@ -1,17 +1,11 @@
 package com.example.financialportfolioapp.domain.entities
 
-import java.util.Date
-
 interface AssetInterface {
     val id: Int
     val name: String
-    var date: Date
-    var price: Double
 }
 
 data class Asset(
     override val id: Int,
     override val name: String,
-    override var date: Date,
-    override var price: Double,
 ) : AssetInterface
