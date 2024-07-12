2cec31ca82ea5907a31e246a82062e8fe1586da3
arkadi4 <arkadi40102@gmail.com>
2024-07-12 14:08:21 +0300

    currencies enum and Price class were added

--- app/src/main/java/com/example/financialportfolioapp/data/DataSample.kt
+++ app/src/main/java/com/example/financialportfolioapp/data/DataSample.kt
@@ -1,10 +1,12 @@
 package com.example.financialportfolioapp.data
 
+import com.example.financialportfolioapp.domain.entities.AppCurrencies
 import com.example.financialportfolioapp.domain.entities.Asset
 import com.example.financialportfolioapp.domain.entities.AssetInterface
 import com.example.financialportfolioapp.domain.entities.Bond
 import com.example.financialportfolioapp.domain.entities.Cash
 import com.example.financialportfolioapp.domain.entities.PortfolioItemInterface
+import com.example.financialportfolioapp.domain.entities.Price
 import com.example.financialportfolioapp.domain.entities.Stock
 import java.util.Calendar
 
@@ -15,20 +17,35 @@ object DataSample {
         Asset(3, "Minsk Tractors"),
         Asset(4, "Lufthansa"),
         Asset(5, "GP Morgan"),
-        Asset(6, "Columbia Pictures"),
+        Asset(6, "Columbia Pictures")
     )
 
     var portfolioItemsList: MutableList<PortfolioItemInterface> = mutableListOf(
         Cash(
-            1, "USD", 15.0, 3.20, "BYN", Calendar.getInstance()
+            1,
+            "USD",
+            15.0,
+            Price(
+                1.0, AppCurrencies.USD, Calendar.getInstance()
+            )
         ),
         Stock(
-            2, "Minsk Tractors", 20.0, 5_000.0, "BYN",
-            Calendar.getInstance(),
+            2,
+            "Minsk Tractors",
+            20.0,
+            Price(
+                500.0, AppCurrencies.BYN, Calendar.getInstance()
+            )
         ),
         Bond(
-            3, "GP Morgan", 30.0, 50_000.0, "BYN",
-            Calendar.getInstance(), 51_000.0, 1_000.0,
-        ),
+            3,
+            "GP Morgan",
+            300.0,
+            Price(
+                30.0, AppCurrencies.BYN, Calendar.getInstance()
+            ),
+            350.0,
+            50.0
+        )
     )
 }
--- app/src/main/java/com/example/financialportfolioapp/domain/entities/PortfolioItem.kt
+++ app/src/main/java/com/example/financialportfolioapp/domain/entities/PortfolioItem.kt
@@ -2,51 +2,80 @@ package com.example.financialportfolioapp.domain.entities
 
 import java.util.Calendar
 
-interface PortfolioItemInterface : AssetInterface {
-    var amount: Double
-    var price: Double
-    var currency: String
+const val BynToByn = 1.0
+const val BynToUSD = 0.32
+const val UsdToByn = 32.0
+const val UsdToUsd = 1.0
+
+enum class AppCurrencies(
+    val currencyName: String,
+    var exchangeRatio: MutableMap<String, Double>
+) {
+    BYN(
+        "BYN",
+        mutableMapOf(
+            "BYN" to BynToByn,
+            "USD" to BynToUSD
+        )
+    ),
+    USD(
+        "USD",
+        mutableMapOf(
+            "BYN" to UsdToByn,
+            "USD" to UsdToUsd
+        )
+    ),
+//    RUB,
+//    CNY
+}
+
+class Price(
+    var priceValue: Double,
+    var priceCurrency: AppCurrencies,
     var dateOfLastPriceUpdate: Calendar
-    var priceHistory: MutableMap<Calendar, Double>
+) {
+    var priceString: String = "$priceValue ${priceCurrency.currencyName}"
+    var history: MutableMap<Calendar, String> = mutableMapOf(dateOfLastPriceUpdate to priceString)
+
+    fun changeToAnotherCurrency(anotherCurrency: AppCurrencies) {
+        priceValue = priceValue * anotherCurrency.exchangeRatio.get(anotherCurrency.currencyName)!!
+        priceCurrency = anotherCurrency
+    }
+
+    fun updatePrice(newDateOfLastPriceUpdate: Calendar, newPriceValue: Double) {
+        dateOfLastPriceUpdate = newDateOfLastPriceUpdate
+        priceString = "$newPriceValue ${priceCurrency.currencyName}"
+        history.put(newDateOfLastPriceUpdate, priceString)
+    }
+}
+
+interface PortfolioItemInterface : AssetInterface {
+    val amount: Double
+    val price: Price
 }
 
 data class Cash(
     override val id: Int,
     override val name: String,
-    override var amount: Double,
-    override var price: Double,
-    override var currency: String,
-    override var dateOfLastPriceUpdate: Calendar
-) : PortfolioItemInterface {
-    override var priceHistory: MutableMap<Calendar, Double> =
-        mutableMapOf(dateOfLastPriceUpdate to price)
-}
+    override val amount: Double,
+    override val price: Price
+) : PortfolioItemInterface
 
 data class Stock(
     override val id: Int,
     override val name: String,
-    override var amount: Double,
-    override var price: Double,
-    override var currency: String,
-    override var dateOfLastPriceUpdate: Calendar,
-) : PortfolioItemInterface {
-    override var priceHistory: MutableMap<Calendar, Double> =
-        mutableMapOf(dateOfLastPriceUpdate to price)
-}
+    override val amount: Double,
+    override val price: Price
+) : PortfolioItemInterface
 
 data class Bond(
     override val id: Int,
     override val name: String,
-    override var amount: Double,
-    override var price: Double,
-    override var currency: String,
-    override var dateOfLastPriceUpdate: Calendar,
-    var futurePrice: Double,
-    var yieldToMaturity: Double,
-) : PortfolioItemInterface {
-    override var priceHistory: MutableMap<Calendar, Double> =
-        mutableMapOf(dateOfLastPriceUpdate to price)
-}
+    override val amount: Double,
+    override val price: Price,
+    val futurePrice: Double,
+    val yieldToMaturity: Double
+) : PortfolioItemInterface
 
 interface MetaInfoInterface {
     val country: String
