4684c20754e05c873360fda3667ae435ba32aef3
Kirill Startsev <kirill.star.1408@gmail.com>
2024-07-12 18:38:26 +0300

    changes

---
+++ app/src/main/java/com/example/financialportfolioapp/presentation/assetList/AssetListViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.financialportfolioapp.presentation.assetList
+
+class AssetListViewModel {
+}
---
+++ app/src/main/java/com/example/financialportfolioapp/presentation/rv/AssetListAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.financialportfolioapp.presentation.rv
+
+class AssetListAdapter {
+}
