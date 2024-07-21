700bcbfca6de995d1044527991d84e690867ae8f
Kirill Startsev <kirill.star.1408@gmail.com>
2024-07-21 21:14:06 +0300

    detail fragment, data binding, repository + impl

--- app/src/main/java/com/example/financialportfolioapp/presentation/assetDetails/AssetDetailsFragment.kt
+++ app/src/main/java/com/example/financialportfolioapp/presentation/assetDetails/AssetDetailsFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.financialportfolioapp.databinding.FragmentAssetDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @AndroidEntryPoint
 class AssetDetailsFragment : Fragment() {
@@ -14,6 +18,8 @@ class AssetDetailsFragment : Fragment() {
     private var _binding: FragmentAssetDetailsBinding? = null
     private val binding get() = _binding!!
 
+    private val assetDetailViewModel: AssetDetailsViewModel by viewModels()
+    private val args: AssetDetailsFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,6 +29,22 @@ class AssetDetailsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val assetId = args.assetId
+
+        assetDetailViewModel.loadItem(assetId).observe(viewLifecycleOwner) { item ->
+
+            val calendar = item.price.dateOfLastPriceUpdate
+            val dateFormat = SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault())
+            val dateString = dateFormat.format(calendar.time)
+            binding.nameValue.text = item.name
+            binding.amountValue.text = item.amount.toString()
+            binding.priceValue.text = item.price.priceValue.toString()
+            binding.currencyValue.text = item.price.priceCurrency.toString()
+            binding.lastPriceUpdateValue.text = dateString
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
---
+++ app/src/main/java/com/example/financialportfolioapp/presentation/assetDetails/AssetDetailsViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.financialportfolioapp.presentation.assetDetails
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.financialportfolioapp.domain.entities.PortfolioItemInterface
+import com.example.financialportfolioapp.domain.repository.PortfolioItemRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AssetDetailsViewModel @Inject constructor(
+    private val portfolioItemRepository: PortfolioItemRepository
+) : ViewModel() {
+
+    private val _item = MutableLiveData<PortfolioItemInterface>()
+    val item: LiveData<PortfolioItemInterface> get() = _item
+
+    fun loadItem(assetId: Int): LiveData<PortfolioItemInterface> {
+        val item = portfolioItemRepository.getItemById(assetId)
+        _item.value = item
+        return _item
+    }
+}
