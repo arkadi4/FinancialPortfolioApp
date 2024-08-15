e7e7f56f641ef0ec5f1ca4066358e09dc5179d5e
arkadi4 <arkadi40102@gmail.com>
2024-08-15 21:50:30 +0300

    fix #1 after review

--- app/src/main/java/com/example/financialportfolioapp/data/repository/SettingsStorageRepositoryImpl.kt
+++ app/src/main/java/com/example/financialportfolioapp/data/repository/SettingsStorageRepositoryImpl.kt
@@ -1,13 +1,10 @@
 package com.example.financialportfolioapp.data.repository
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import com.example.financialportfolioapp.domain.repository.SettingsStorageRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
@@ -15,15 +12,13 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
 class SettingsStorageRepositoryImpl @Inject constructor(
-    @ApplicationContext private val context: Context
+    private val dataStore: DataStore<Preferences>
 ) : SettingsStorageRepository {
-    private val Context.dataStore: DataStore<Preferences>
-        by preferencesDataStore(name = SETTINGS_STORAGE_NAME)
     private val defaultCurrencyKey = stringPreferencesKey(DEFAULT_CURRENCY_KEY)
 
     override suspend fun getSettings(): String {
         return withContext(Dispatchers.IO) {
-            context.dataStore.data.map { prefs ->
+            dataStore.data.map { prefs ->
                 prefs[defaultCurrencyKey] ?: "BYN"
             }.first()
         }
@@ -31,14 +26,13 @@ class SettingsStorageRepositoryImpl @Inject constructor(
 
     override suspend fun setSettings(newCurrency: String) {
         withContext(Dispatchers.IO) {
-            context.dataStore.edit { preferences ->
+            dataStore.edit { preferences ->
                 preferences[defaultCurrencyKey] = newCurrency
             }
         }
     }
 
     companion object {
-        const val SETTINGS_STORAGE_NAME = "Settings storage"
         const val DEFAULT_CURRENCY_KEY = "default currency"
     }
 }
