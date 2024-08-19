a946fdcb68d53b13edd5e368f0f3f4c6b3ccf6e4
Kirill Startsev <kirill.star.1408@gmail.com>
2024-08-19 17:37:02 +0300

    changes

---
+++ app/src/main/java/com/example/financialportfolioapp/presentation/createStock/CreateStockViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.financialportfolioapp.presentation.createStock
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.financialportfolioapp.domain.entities.Price
+import com.example.financialportfolioapp.domain.repository.PortfolioItemRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CreateStockViewModel @Inject constructor(
+    private val portfolioItemRepository: PortfolioItemRepository
+):ViewModel() {
+    fun addStock(name: String, amount: Double, price: Price, dividends: Double) {
+        viewModelScope.launch {
+            portfolioItemRepository.addStock(name, amount, price, dividends)
+        }
+
+
+
+    }
+}
